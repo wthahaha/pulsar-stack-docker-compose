08c9d670882c9d7f219fa3c933acb03429491990
Your Name <you@example.com>
2022-08-30 23:20:30 +0800

    update

---
+++ init.sh
@@ -0,0 +1,9 @@
+mkdir -p zoo1/{data,log}
+mkdir -p zoo2/{data,log}
+mkdir -p zoo3/{data,log}
+mkdir -p bookie1/{data,log}
+mkdir -p bookie2/{data,log}
+mkdir -p bookie3/{data,log}
+mkdir -p broker1/{data,log}
+mkdir -p broker2/{data,log}
+mkdir -p proxy/{data,log}
